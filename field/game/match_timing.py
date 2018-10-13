96a3d81996f4fbfac899cc52b01b08c96cc5cb67
Evan Pratten <ewpratten@gmail.com>
2018-10-13 15:17:01 -0400

    Create match_timing.py

---
+++ field/game/match_timing.py
@@ -0,0 +1,11 @@
+# Source: https://github.com/Team254/cheesy-arena/blob/master/game/match_timing.go
+
+duration = {
+  "Warmup" : 3
+	"Auto" : 15
+	"Pause" : 2
+	"Teleop" : 135
+	"Endgame_legnth" : 30
+}
+ def gameLegnth():
+  return (duration["Warmup"] + duration["Auto"] + duration["Pause"] + duration["Teleop"])
