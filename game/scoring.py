aa7517234483343b56428a514bb08959daddaf48
Evan Pratten <ewpratten@gmail.com>
2019-04-17 10:43:05 -0400

    cleanup

--- game/scoring.py
+++
@@ -1,7 +0,0 @@
-increments = {
-    "primary": {"name": "+1", "value": 1},
-    "secondary": {"name": "+2", "value": 2}
-}
-
-# This is a list to allow multiple files to access it.. Python is weird
-score = [0]
