0d07a6d299d73b48864835ca89fb08a506a0d62c
Evan Pratten <ewpratten@gmail.com>
2019-04-20 14:23:40 -0400

    cleaned up un-used files

--- docs/om5pan.md
+++
@@ -1,3 +0,0 @@
-go here: https://downloads.openwrt.org/chaos_calmer/15.05/ar71xx/generic/openwrt-15.05-ar71xx-generic-om5p-squashfs-factory.bin
-
-use this to flash: https://kb.datto.com/hc/en-us/articles/360024058011-Access-Point-Firmware-Downloads
--- websocket/websocket.py
+++ websocket/websocket.py
@@ -6,7 +6,6 @@ import json
 config = json.load(open("./config.json", "r"))
 
 server = WebsocketServer(config["websocket_port"], host='0.0.0.0')
-server.set_fn_new_client(lambda c, _: print(c))
 
 def RunWrapper():
     ws_thread = Thread(target=server.run_forever)
